152a621ede2c01c14edb7a1b3f12589289a35199
DenisGridin <denisgridin6@gmail.com>
2020-04-11 14:07:24 +0300

    fix gitignore

--- helpers/constants.js
+++
@@ -1,2 +0,0 @@
-password = 'denisgridin'
-module.exports = { database: 'mongodb+srv://denisgridin:' + password + '@time-management-cluster-zsrvx.mongodb.net/time-management-database?retryWrites=true&w=majority' }
