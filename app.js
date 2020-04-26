3cb12e5060acf6ee8bae28fddf19bbdfa1218f54
DenisGridin <denisgridin6@gmail.com>
2020-04-26 19:38:28 +0300

    fix cors

--- app.js
+++ app.js
@@ -36,7 +36,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use(function(req, res, next) {
 //   next(createError(404));
 // });
-
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", '*');
+	res.header("Access-Control-Allow-Credentials", true);
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+	next();
+});
 
 // routes
 var usersRouter = require('./components/users/RouteUsers');
