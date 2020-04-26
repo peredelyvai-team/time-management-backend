8b9e0ba4969a92995fdeb015622caf161aef85d8
DenisGridin <denisgridin6@gmail.com>
2020-04-26 20:03:57 +0300

    add cors

--- app.js
+++ app.js
@@ -31,19 +31,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+var cors = require('cors')
+app.use(cors())
 // // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
 //   next(createError(404));
 // });
-app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", '*');
-	res.header("Access-Control-Allow-Credentials", true);
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-	res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-	next();
-});
-
 // routes
 var usersRouter = require('./components/users/RouteUsers');
 var tokensRouter = require('./components/tokens/RoutesToken');
@@ -60,7 +53,6 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
